1b69b6181e71b44bdf947ad89b2143a798fdc49b
danielsanchezrodriguez <srdaniel83@gmail.com>
2015-12-16 15:16:52 +0100

    Hasta el 30 incluido

--- src/forEjercicios/Ejercicio29.java
+++ src/forEjercicios/Ejercicio29.java
@@ -13,14 +13,40 @@ public class Ejercicio29 {
 
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Introduce el primer lado del triángulo: ");
-		int x = scan.nextInt();
-		System.out.print("Introduce el segundo lado del triángulo: ");
-		int y = scan.nextInt();
-		System.out.print("Introduce el tercer lado del triángulo: ");
-		int z = scan.nextInt();
+		System.out.print("¿Cuántos triángulos quieres introducir?: ");
+		int n;
+		n = scan.nextInt();
+		double x;
+		double y;
+		double z;
+		String mensaje = null;
+		int equilatero = 0;
+		int isosceles = 0;
+		int escaleno = 0;
 
-		
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Introduce el primer lado del triángulo: ");
+			x = scan.nextInt();
+			System.out.print("Introduce el segundo lado del triángulo: ");
+			y = scan.nextInt();
+			System.out.print("Introduce el tercer lado del triángulo: ");
+			z = scan.nextInt();
+
+			if (x == y && x == z) {
+				mensaje = "El triángulo introducido es equilátero";
+				equilatero++;
+			} else if (x == y) {
+				mensaje = "El triángulo introducido es isósceles";
+				isosceles++;
+			} else if (x != y && x != z && y != z) {
+				mensaje = "El triángulo introducido es escaleno";
+				escaleno++;
+			}
+			System.out.println(mensaje);
+		}
+		System.out.println("La cantidad de triángulos equiláteros es: "
+				+ equilatero + ". "
+				+ "La cantidad de triángulos isósceles es: " + isosceles + ". "
+				+ "La cantidad de triángulos escalenos es: " + escaleno + ".");
 	}
-
 }
--- src/forEjercicios/Ejercicio30.java
+++ src/forEjercicios/Ejercicio30.java
@@ -10,12 +10,31 @@ public class Ejercicio30 {
 
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Introduce la coordenada 'X': ");
-		int x = scan.nextInt();
-		System.out.println("Introduce la coordenada 'Y': ");
-		int y = scan.nextInt();
 
-		
+		System.out
+				.print("¿Cuántas coordenadas 'X' e 'Y' quieres introducir?: ");
+		int n;
+		n = scan.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+
+			System.out.print("Introduce la coordenada 'X':");
+			int x = scan.nextInt();
+			System.out.print("Introduce la coordenada 'Y':");
+			int y = scan.nextInt();
+
+			if (x > 0 && y > 0) {
+				System.out
+						.println("Has introducido las coordenadas del primer cuadrante");
+			} else if (x < 0 && y > 0) {
+				System.out
+						.println("Has introducido las coordenadas del segundo cuadrante");
+			} else if (x > 0 && y < 0) {
+				System.out
+						.println("Has introducido las coordenadas del tercer cuadrante");
+			} else
+				System.out
+						.println("Has introducido las coordenadas del cuarto cuadrante");
+		}
 	}
-
 }
